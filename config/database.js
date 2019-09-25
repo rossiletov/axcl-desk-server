da1b5f5e380d072a5917413dd63a0c1e4b373036
rossiletov <i@rossiletov.com>
2019-09-25 18:01:47 +0300

    init commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'secret':'nodeauthsecret',
+  'database': 'mongodb://localhost/tiledesk',
+  'databaselogs': 'mongodb://localhost/tiledesk-logs',
+  'databasetest': 'mongodb://localhost/tiledesk-test'
+  
+};
